2d046e621b8341d27f0daa34b2e1952daed9b888
Luca Laranjeira <laranjeiralucas17@gmail.com>
2025-01-31 16:44:27 -0300

    View da Pagina de transportadoras

--- src/views/transportadorasList.tsx
+++ src/views/transportadorasList.tsx
@@ -1,14 +1,49 @@
-import react from "react";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from '../App.tsx'
+import { table } from "console";
 
 
 
 
-const TransportadorasLista = () => {
-    return(
 
-     <h1>Transportadoas</h1>
-    
-    )
-};
-  
-  export default TransportadorasLista;
+
+
+function TransportadorasLista(){
+
+  return(
+    <div className="TransportadorasBody">
+      <header>
+        <h1>Transportadoras</h1>
+      </header>
+      <div className="TransportadoraConteudo">
+      <Lista/>
+      </div>
+    </div>
+  )
+
+} 
+
+export default TransportadorasLista;
+
+function Lista() {  
+
+  return(
+
+    <div className="ListaTransportadoras">
+      <div className="cardLista"></div>
+      <div className="cardLista"></div>
+      <div className="cardLista"></div>
+      <div className="cardLista"></div>
+      <div className="cardLista"></div>
+      <div className="cardLista"></div>
+      <div className="cardLista"></div>
+      <div className="cardLista"></div>
+      <div className="cardLista"></div>
+      <div className="cardLista"></div>
+
+    </div>
+
+
+  )
+}
