ebe07740b13daa93bc0e0cd3dd06db9e146fe3a4
Luca Laranjeira <laranjeiralucas17@gmail.com>
2025-02-03 14:33:30 -0300

    Mudanças no layout e adição do input de lista (S/Backend)

--- src/views/transportadorasList.tsx
+++ src/views/transportadorasList.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 import App from '../App.tsx'
 import { table } from "console";
@@ -28,10 +28,29 @@ export default TransportadorasLista;
 
 function Lista() {  
 
+  const Transportadora = { id: 1, nome: "ABT LOG", motorista: "Jose Alberto", rg_motorista: "0000000", entrada: "03/02/2025 09:34" }
+
+
   return(
 
     <div className="ListaTransportadoras">
-      <div className="cardLista"></div>
+      <div className="cardLista">
+        <form action="" method="post">
+          <label htmlFor="">Transportadora</label>
+          <input type="text" value={Transportadora.nome} disabled/>
+          <label htmlFor="">Motorista</label>
+          <input type="text" value={Transportadora.motorista} disabled/>
+          <label htmlFor="">Placa</label>
+          <input type="text" value={Transportadora.rg_motorista} disabled/>
+          <label htmlFor="">Entrada</label>
+          <input type="text" value={Transportadora.entrada} disabled />
+          <label htmlFor="">Saida</label>
+          <input type="datetime-local" />
+          <button id="btn-saida" type="submit" value={Transportadora.id}>
+            Registrar Saida
+          </button>
+        </form>
+      </div>
       <div className="cardLista"></div>
       <div className="cardLista"></div>
       <div className="cardLista"></div>
