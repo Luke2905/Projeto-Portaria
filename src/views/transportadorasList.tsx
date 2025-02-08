f1800467efde97da80c25b15f5cb5afea5a62232
Luke2905 <laranjeiralucas17@gmail.com>
2025-02-08 00:42:20 -0300

    Conexao com API e exibição dos dados do DB

--- src/views/transportadorasList.tsx
+++ src/views/transportadorasList.tsx
@@ -1,12 +1,8 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import App from '../App.tsx'
 import { table } from "console";
-
-
-
-
-
+import api from "../service/api.ts"; //-> importação da conexão com a API do backend
 
 
 function TransportadorasLista(){
@@ -28,49 +24,41 @@ export default TransportadorasLista;
 
 function Lista() {  
 
-  const Transportadora = [
-    {
-       id: 1, 
-      nome: "ABT LOG", 
-      motorista: "Jose Alberto", 
-      rg_motorista: "0000000", 
-      entrada: "03/02/2025 09:34" 
-    },
-    {
-      id: 2, 
-      nome: "TRANS REID", 
-      motorista: "Alberto", 
-      rg_motorista: "88888888", 
-      entrada: "03/02/2025 09:34" 
-    },
-    {
-      id: 3, 
-      nome: "TRANS DIADEMA", 
-      motorista: "Solano Lopez", 
-      rg_motorista: "989898989", 
-      entrada: "07/02/2025 12:00" 
-    }
+  const [transportadora, setTransportadora] = useState([]) //-> useState é um hook ( funcionalidade do React) que vai modificar o "estado" de uma variavel exibida na tela
 
+  let Transportadora = []
 
-  ]
+  async function getTransportadoras() { //-> Função para acessar o backend
+    
+  const transportadorasApi = await api.get('/transportadoras')
+  
+  setTransportadora(transportadorasApi.data)   //-> seleciona apenas o campo data da requisiçao do backend
+
+  }
+
+  useEffect(() =>{
+
+    getTransportadoras()
+
+  }, [])
 
 
   return(
   
     <div className="ListaTransportadoras">
 
-          {Transportadora.map (transportadora => (  //-> função map: mapeia os componentes do array e exibe todos na tela
+          {transportadora.map (transportadora => (  //-> função map: mapeia os componentes do array e exibe todos na tela
 
             <div className="cardLista" key={transportadora.id} /*-> Key é o componente unico para identificar cada card*/>
             <form action="" method="post">
               <label htmlFor="">Transportadora</label>
-              <input type="text" value={transportadora.nome} disabled/>
+              <input type="text" value={transportadora.transportadora} disabled/>
               <label htmlFor="">Motorista</label>
               <input type="text" value={transportadora.motorista} disabled/>
               <label htmlFor="">Placa</label>
-              <input type="text" value={transportadora.rg_motorista} disabled/>
+              <input type="text" /*value={transportadora.rg_motorista}*/ disabled/>
               <label htmlFor="">Entrada</label>
-              <input type="text" value={transportadora.entrada} disabled />
+              <input type="text" value={transportadora.dth_entrada} disabled />
               <label htmlFor="">Saida</label>
               <input type="datetime-local" />
               <button id="btn-saida" type="submit">
